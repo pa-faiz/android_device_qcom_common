e4deb7572a87a3a629fd315521624fca76bc35f4
Fazil Sheik <fazil.sheik96@gmail.com>
2024-10-15 11:56:02 +0530

    adreno-msmnile: Sync extract-utils with template

--- vendor/adreno-msmnile/setup-makefiles.sh
+++ vendor/adreno-msmnile/setup-makefiles.sh
@@ -17,8 +17,14 @@
 
 set -e
 
-# Required!
+# If we're being sourced by the common script that we called,
+# stop right here. No need to go down the rabbit hole.
+if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
+    return
+fi
+
 export COMPONENT=adreno-msmnile
 export VENDOR=qcom/common/vendor
+export VENDOR_COMMON=${VENDOR}
 
 "../setup-makefiles.sh" "$@"
